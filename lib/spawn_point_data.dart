defecedbdc0d6c116340c6882dbff6422a54072d
Invis1ble_ <1434045601@qq.com>
2025-12-14 23:36:47 +0800

    added all maps spawn points updated version code

--- lib/spawn_point_data.dart
+++ lib/spawn_point_data.dart
@@ -90,4 +90,119 @@ const Map<String, MapSpawnConfig> spawnPointData = {
       SpawnPoint(5, 0.0807, 0.7155)
     ],
   ),
+  'anubis': MapSpawnConfig(
+    ctSpawns: [
+      SpawnPoint(1, 0.4438, 0.214),
+      SpawnPoint(2, 0.4293, 0.2055),
+      SpawnPoint(3, 0.4153, 0.214),
+      SpawnPoint(4, 0.4042, 0.2255),
+      SpawnPoint(5, 0.4512, 0.2275),
+    ],
+    tSpawns: [
+      SpawnPoint(1, 0.4502, 0.9405),
+      SpawnPoint(2, 0.4773, 0.897),
+      SpawnPoint(3, 0.4938, 0.896),
+      SpawnPoint(4, 0.4542, 0.9175),
+      SpawnPoint(5, 0.4637, 0.906),
+      SpawnPoint(6, 0.4738, 0.9195),
+      SpawnPoint(7, 0.4983, 0.9095),
+      SpawnPoint(8, 0.5012, 0.9265),
+      SpawnPoint(9, 0.4452, 0.9259),
+      SpawnPoint(10, 0.4661, 0.93),
+      SpawnPoint(11, 0.4866, 0.9336),
+    ],
+  ),
+  'ancient': MapSpawnConfig(
+    ctSpawns: [
+      SpawnPoint(1, 0.5366, 0.0956),
+      SpawnPoint(2, 0.5191, 0.0894),
+      SpawnPoint(3, 0.5003, 0.0894),
+      SpawnPoint(4, 0.4797, 0.0912),
+      SpawnPoint(5, 0.4684, 0.1031),
+    ],
+    tSpawns: [
+      SpawnPoint(1, 0.5091, 0.8788),
+      SpawnPoint(2, 0.4816, 0.8781),
+      SpawnPoint(3, 0.4553, 0.8775),
+      SpawnPoint(4, 0.4972, 0.8575),
+      SpawnPoint(5, 0.4716, 0.855),
+    ],
+  ),
+  'nuke': MapSpawnConfig(
+    ctSpawns: [
+      SpawnPoint(1, 0.8375, 0.4456),
+      SpawnPoint(2, 0.8442, 0.4606),
+      SpawnPoint(3, 0.8364, 0.4728),
+      SpawnPoint(4, 0.8531, 0.4467),
+      SpawnPoint(5, 0.8531, 0.4733),
+    ],
+    tSpawns: [
+      SpawnPoint(1, 0.2286, 0.5456),
+      SpawnPoint(2, 0.2114, 0.5361),
+      SpawnPoint(3, 0.2103, 0.5544),
+      SpawnPoint(4, 0.2184, 0.5318),
+      SpawnPoint(5, 0.2275, 0.5615),
+      SpawnPoint(6, 0.2178, 0.545),
+      SpawnPoint(7, 0.2204, 0.5529),
+      SpawnPoint(8, 0.2287, 0.5535),
+    ],
+  ),
+  'overpass': MapSpawnConfig(
+    ctSpawns: [
+      SpawnPoint(1, 0.496, 0.1973),
+      SpawnPoint(2, 0.4844, 0.1935),
+      SpawnPoint(3, 0.4729, 0.1888),
+      SpawnPoint(4, 0.4921, 0.1835),
+      SpawnPoint(5, 0.4798, 0.1808)
+    ],
+    tSpawns: [
+      SpawnPoint(1, 0.6548, 0.9373),
+      SpawnPoint(2, 0.6325, 0.9173),
+      SpawnPoint(3, 0.6679, 0.9485),
+      SpawnPoint(4, 0.6217, 0.9277),
+      SpawnPoint(5, 0.6206, 0.9135),
+      SpawnPoint(6, 0.6575, 0.9535),
+      SpawnPoint(7, 0.6406, 0.9427),
+      SpawnPoint(8, 0.644, 0.9558),
+      SpawnPoint(9, 0.6444, 0.9242),
+      SpawnPoint(10, 0.6279, 0.9377),
+      SpawnPoint(11, 0.6344, 0.9277)
+    ],
+  ),
+  'vertigo': MapSpawnConfig(
+    ctSpawns: [
+      SpawnPoint(1, 0.5648, 0.1935),
+      SpawnPoint(2, 0.5503, 0.2035),
+      SpawnPoint(3, 0.4828, 0.2225),
+      SpawnPoint(4, 0.4953, 0.1985),
+      SpawnPoint(5, 0.5162, 0.1985)
+    ],
+    tSpawns: [
+      SpawnPoint(1, 0.4308, 0.7035),
+      SpawnPoint(2, 0.4078, 0.77),
+      SpawnPoint(3, 0.4233, 0.7615),
+      SpawnPoint(4, 0.4243, 0.7405),
+      SpawnPoint(5, 0.4158, 0.721)
+    ],
+  ),
+  'train': MapSpawnConfig(
+    ctSpawns: [
+      SpawnPoint(1, 0.8835, 0.792),
+      SpawnPoint(2, 0.9265, 0.791),
+      SpawnPoint(3, 0.9065, 0.825),
+      SpawnPoint(4, 0.9415, 0.824),
+      SpawnPoint(5, 0.9585, 0.856),
+      SpawnPoint(6, 0.9225, 0.855),
+      SpawnPoint(7, 0.8735, 0.851),
+      SpawnPoint(8, 0.9045, 0.777),
+    ],
+    tSpawns: [
+      SpawnPoint(1, 0.0635, 0.171),
+      SpawnPoint(2, 0.0975, 0.168),
+      SpawnPoint(3, 0.0955, 0.144),
+      SpawnPoint(4, 0.0655, 0.145),
+      SpawnPoint(5, 0.0855, 0.184),
+      SpawnPoint(6, 0.1165, 0.195),
+    ],
+  ),
 };
