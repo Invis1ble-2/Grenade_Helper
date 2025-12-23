4420a889b211a3d0d1a4f5b904f77fb44fa6ffb0
Invis1ble_ <1434045601@qq.com>
2025-12-23 21:36:12 +0800

    add mutex to detect whether app is running

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -4,9 +4,61 @@
 
 #include "flutter_window.h"
 #include "utils.h"
+#include "win32_window.h"
+
+// Single instance mutex name (unique identifier to avoid conflicts)
+static const wchar_t kMutexName[] =
+    L"Global\\GrenadeHelper_SingleInstance_Mutex";
+// Window class name (Flutter runner class)
+static const wchar_t kWindowClassName[] = L"FLUTTER_RUNNER_WIN32_WINDOW";
+// Window title (used to find existing window)
+static const wchar_t kWindowTitle[] = L"Grenade Helper";
+
+// Callback for EnumWindows to find existing app window
+struct EnumWindowsData {
+  HWND foundWindow;
+};
+
+static BOOL CALLBACK EnumWindowsCallback(HWND hwnd, LPARAM lParam) {
+  wchar_t className[256];
+  wchar_t windowTitle[256];
+
+  if (::GetClassNameW(hwnd, className, 256) > 0 &&
+      ::GetWindowTextW(hwnd, windowTitle, 256) > 0) {
+    if (wcscmp(className, kWindowClassName) == 0 &&
+        wcscmp(windowTitle, kWindowTitle) == 0) {
+      EnumWindowsData *data = reinterpret_cast<EnumWindowsData *>(lParam);
+      data->foundWindow = hwnd;
+      return FALSE; // Stop enumeration
+    }
+  }
+  return TRUE; // Continue enumeration
+}
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
+  // Single instance check: try to create mutex
+  HANDLE mutex = ::CreateMutexW(nullptr, TRUE, kMutexName);
+  if (mutex == nullptr) {
+    // Failed to create mutex, exit
+    return EXIT_FAILURE;
+  }
+
+  if (::GetLastError() == ERROR_ALREADY_EXISTS) {
+    // Another instance is already running
+    // Use EnumWindows to find window even if hidden
+    EnumWindowsData data = {nullptr};
+    ::EnumWindows(EnumWindowsCallback, reinterpret_cast<LPARAM>(&data));
+
+    if (data.foundWindow != nullptr) {
+      // Send custom message to request window to show itself
+      ::SendMessageW(data.foundWindow, WM_SHOW_ME, 0, 0);
+    }
+    // Release mutex and exit
+    ::CloseHandle(mutex);
+    return EXIT_SUCCESS;
+  }
+
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
@@ -19,8 +71,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   flutter::DartProject project(L"data");
 
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
+  std::vector<std::string> command_line_arguments = GetCommandLineArguments();
 
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
@@ -39,5 +90,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   }
 
   ::CoUninitialize();
+
+  // Release single instance mutex
+  ::CloseHandle(mutex);
   return EXIT_SUCCESS;
 }
